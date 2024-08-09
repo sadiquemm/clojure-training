9cfecd8b8fd72a16cfb07c69aa27c5edec8d42cd
Sadique <sadiq4mm@gmail.com>
2024-08-09 12:08:40 +0530

    Collection

---
+++ collection/src/collection/core.clj
@@ -0,0 +1,20 @@
+(ns collection.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+
+;;list
+(def list1 '(1 2 3))
+(println list1)
+
+;;set
+(def set1 #{1 3 5})
+(println set1)
+
+;;vector
+(def vect [1 0 3])
+(println vect)
+  
+  )
