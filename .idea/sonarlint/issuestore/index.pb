0f6a157440ccc7b15b0e30c9493a223023751dff
sadiquemm <sadique@perfectzdigital.com>
2024-08-15 16:26:08 +0530

    Conditions example

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+R
+"conditions/src/conditions/core.clj,4/5/4553cbcee7e3e22a25731716f2572f7aee030d57
--- conditions/src/conditions/core.clj
+++ conditions/src/conditions/core.clj
@@ -1,7 +1,49 @@
 (ns conditions.core
   (:gen-class))
+(declare condition-if)
+(declare case-test)
+(declare cond-test)
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+
+  ;;if condition
+  ;;(condition-if)
+
+  ;;Case example
+  ;;(case-test)
+
+  ;;Cond example
+  ;;(cond-test)
+  )
+
+
+(defn cond-test
+  []
+  (def var 100)
+  (cond
+    (<= var 5) (println "less than 10")
+    (<= var 20) (println "less than 20")
+    (<= var 30) (println "less than 30")
+    :else
+    (println "else cond")
+    )
+
+  )
+
+(defn case-test
+  []
+  (def var "1")
+  (case var
+        "2" (println "two")
+        "1" (println "one")
+        )
+  )
+(defn condition-if
+  []
+  (if (= 2 2)
+    (println "True")
+    (println "False")
+    )
+  )
+
